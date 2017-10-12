be965b8c8d37fa827cb121739f5a5b30dc853225
cattlelin <cattlelin@springwoods.com>
2017-10-12 18:30:01 +0800

    modify roductinfo page

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -141,8 +141,28 @@ export function fetchGetProducts(opts) {
         'queryProductInfoListRequest.productId': opts.productId
     }));
 }
+export function fetchSaveProductLinks(opts) {
+    console.log('opts是什么？');
+   console.log(opts);
+    return Vue.axios.post('http://10.1.2.151/ctower-mall-c/agriculture/productInfo/addProductInfo.do', qs.stringify({
+        'addProductInfoRequest.chanpmc': opts.name,
+        'addProductInfoRequest.guisd': opts.address,
+        'addProductInfoRequest.fayl': opts.germinate,
+        'addProductInfoRequest.yizcml': opts.transplant,
+        'addProductInfoRequest.dankz': opts.weight,
+        'addProductInfoRequest.caiszq': opts.pluck,
+        'addProductInfoRequest.linksInfoJsonArray': opts.links,
+        
+    }));
+}
 export function fetchKuaidials(opts) {
     return Vue.axios.post('http://10.1.2.151/ctower-mall-c/agriculture/column/dataGrid.do', qs.stringify({
         loginedtoken: opts.token
+    }));
+}
+
+export function fetchLinkTemp(opts){
+    return Vue.axios.post('http://10.1.2.151/ctower-mall-c/agriculture/productInfo/queryWorkLink.do', qs.stringify({
+        'queryWorkLinkRequest.preLinkId': opts.linkId
     }));
 }
